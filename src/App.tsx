9f19a05e74f2d85f2e44a28250f5ed84d1554141
kento-ix <kentoix1105@gmail.com>
2024-11-20 13:53:25 -0800

    irrst commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='bg-black text-blue-400'>
+        hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
